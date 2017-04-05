2d7819d1964ded52bef729e8b22d0e71b916ceb4
MiniMarker <cfmarker@gmail.com>
2017-04-05 09:22:19 +0200

    Mye jobb

--- results.php
+++ results.php
@@ -25,7 +25,12 @@ while ($row = $statement -> fetch(PDO::FETCH_ASSOC)) {
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <meta name="description" content="">
 <meta name="author" content="">
-<link href="results.css" type="text/css" rel="stylesheet">
+    
+<!-- CSS -->
+<link href="css/results.css" type="text/css" rel="stylesheet">
+<link href="OpenSans/stylesheet.css" type="text/css" rel="stylesheet">
+    
+<!-- JAVASCRPT -->
 <script src="map-script.js"></script>
 <script async defer
     src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDom7SHx9ZtEf7dQKyTbbvGjEjkG-aTc6o&callback=initMap">
@@ -35,49 +40,56 @@ while ($row = $statement -> fetch(PDO::FETCH_ASSOC)) {
 
 </head>
 <body>
-    <img id="menu-botton" src="img/menu.png">
-    <div id="page-title">
-        <h1>Resturanter</h1>
-    </div>
-    <div id="outer-box">
-        <div id="left-inner-box">
-            
-            <?php foreach($resturants as $resturant) { ?>
-            <a href="<?= $resturant["link-path"] ?>" >
-            <div id="card">
-                    <img class="card-img" src="<?= $resturant["img_path"] ?>" >
-                
-                <h1 class="card-title"><?= $resturant["name"] ?></h1>
-                
-                <?php if ($resturant["rating"] == 5) { ?>  
-                    <img class="stars" src="img/stars/5-stars.png">
-                        
-                <?php } elseif ($resturant["rating"] == 4) { ?>
-                    <img class="stars" src="img/stars/4-stars.png">
-                
-                <?php } elseif ($resturant["rating"] == 3) { ?>
-                    <img class="stars" src="img/stars/3-stars.png">
-                
-                <?php } elseif ($resturant["rating"] == 2) { ?>
-                    <img class="stars" src="img/stars/2-stars.png">
-                
-                <?php } elseif ($resturant["rating"] == 1) { ?>
-                    <img class="stars" src="img/stars/1-star.png">
-                
-                <?php } ?>
-                
-                <div class="card-description">
-                <p ><?= $resturant["description"] ?> </p>
-                </div>
-            </div>
-            </a>
-            
-            
-            <?php } ?>
-        </div>
+    <div id="container">
+        <img id="background" src="img/background.png">
         
-        <div id="right-inner-box">
-            <div id="map"></div>
+        <a href="index.html">
+            <img id="menu-botton" src="img/menu.png">
+        </a>
+
+        <div id="page-title">
+            <h1>Resturanter</h1>
+        </div>
+        <div id="outer-box">
+            <div id="left-inner-box">
+
+                <?php foreach($resturants as $resturant) { ?>
+
+                <div class="cards">
+                    <img class="card-img" src="<?= $resturant["img_path"] ?>" >
+
+                    <a href="<?= $resturant["link-path"] ?>" >
+                        <h1 class="card-title"><?= $resturant["name"] ?></h1>
+                    </a>
+
+                    <?php if ($resturant["rating"] == 5) { ?>  
+                        <img class="card-stars" src="img/stars/5-stars.png">
+
+                    <?php } elseif ($resturant["rating"] == 4) { ?>
+                            <img class="card-stars" src="img/stars/4-stars.png">
+
+                    <?php } elseif ($resturant["rating"] == 3) { ?>
+                          <img class="card-stars" src="img/stars/3-stars.png">
+
+                    <?php } elseif ($resturant["rating"] == 2) { ?>
+                        <img class="card-stars" src="img/stars/2-stars.png">
+
+                    <?php } elseif ($resturant["rating"] == 1) { ?>
+                        <img class="card-stars" src="img/stars/1-star.png">
+
+                    <?php } ?>
+
+                    <div class="card-description">
+                        <p><?= $resturant["description"] ?> </p>
+                    </div>
+                </div>
+
+                <?php } ?>
+            </div>
+
+            <div id="right-inner-box">
+                <div id="map"></div>
+            </div>
         </div>
     </div>
 </body>
