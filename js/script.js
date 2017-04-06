7947267eccddf8614d21771800b9264e38559733
MiniMarker <cfmarker@gmail.com>
2017-04-06 11:31:00 +0200

    div

--- js/script.js
+++ js/script.js
@@ -15,7 +15,7 @@ function showDiv() {
 //Velkommen i flere språk
 function myFunction() {
     "use strict";
-    var x = Math.floor((Math.random() * 10));
+    var x = Math.floor((Math.random() * 11) + 1);
     var y = "(Det står velkommen på ";
     document.getElementById("demo").innerHTML = x;
     document.getElementById("language").innerHTML = y;
@@ -24,50 +24,62 @@ function myFunction() {
         case 0: //Engelsk
             document.getElementById("demo").innerHTML = "Welcome";
             document.getElementById("language").innerHTML = y + "engelsk)";
+            document.getElementById('undertitle').style.top = "-80%";
             break;
         case 1: //Fransk
             document.getElementById("demo").innerHTML = "Bienvenue";
             document.getElementById("language").innerHTML = y + "fransk)";
+            document.getElementById('undertitle').style.top = "-80%";
             break;
         case 2: //Tysk
             document.getElementById("demo").innerHTML = "Willkommen";
             document.getElementById("language").innerHTML = y + "tysk)";
+            document.getElementById('undertitle').style.top = "-80%";
             break;
         case 3: //Italiensk
             document.getElementById("demo").innerHTML = "Benvenuto";
             document.getElementById("language").innerHTML = y + "italiensk)";
+            document.getElementById('undertitle').style.top = "-80%";
             break;
         case 4: //SvensK
             document.getElementById("demo").innerHTML = "Välkommen";
             document.getElementById("language").innerHTML = y + "svensk)";
+            document.getElementById('undertitle').style.top = "-80%";
             break;
         case 5: //Kinesisk (mandarin)
             document.getElementById("demo").innerHTML = "Huānyíng";
             document.getElementById("language").innerHTML = y + "kinesisk)";
+            document.getElementById('undertitle').style.top = "-80%";
             break;
         case 6: //Finsk
             document.getElementById("demo").innerHTML = "Tervetuloa";
             document.getElementById("language").innerHTML = y + "finsk)";
+            document.getElementById('undertitle').style.top = "-80%";
             break;
         case 7: //Islandsk
             document.getElementById("demo").innerHTML = "Velkomin";
             document.getElementById("language").innerHTML = y + "islandsk)";
+            document.getElementById('undertitle').style.top = "-80%";
             break;
         case 8: //Nepalsk
             document.getElementById("demo").innerHTML = "Namaste";
             document.getElementById("language").innerHTML = y + "nepalsk)";
+            document.getElementById('undertitle').style.top = "-80%";
             break;
         case 9: //Portugisisk
             document.getElementById("demo").innerHTML = "Bem-vindo";
             document.getElementById("language").innerHTML = y + "portugisisk)";
+            document.getElementById('undertitle').style.top = "-80%";
             break;
         case 10: //Swahili
             document.getElementById("demo").innerHTML = "Karibu"; 
             document.getElementById("language").innerHTML = y + "swahili)";
+            document.getElementById('undertitle').style.top = "-80%";
             break;
         default:
             document.getElementById("demo").innerHTML = "Velkommen";
-                        document.getElementById("language").innerHTML = y + "norsk)";
+            document.getElementById("language").innerHTML = y + "norsk)";
+            document.getElementById('undertitle').style.top = "-80%";
 
     }
 }
--- php/db-connection.php
+++ php/db-connection.php
@@ -1,7 +1,15 @@
-<?php 
+<?php
 $port = 8889;
 $username = "root";
 $password = "root";
 $name = "eksamen_gruppe17";
+$host = "localhost";
 
-$connection = new PDO("mysql:host=localhost;dbname={$name};port={$port}", $username, $password);
+
+//$port = 3306;
+//$username = "marchr16_cmarker";
+//$password = "08t%Gg@K^GNm";
+//$name = "marchr16_eksamen_gruppe17";
+//$host = "tek.westerdals.no";
+
+$connection = new PDO("mysql:host={$host};dbname={$name};port={$port}", $username, $password);
