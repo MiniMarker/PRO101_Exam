3fbb0d5f2a235b71e1f7160035455a90fc6b6155
MiniMarker <cfmarker@gmail.com>
2017-04-06 10:48:39 +0200

    la til melding under

--- js/script.js
+++ js/script.js
@@ -15,45 +15,60 @@ function showDiv() {
 //Velkommen i flere språk
 function myFunction() {
     "use strict";
-    var x = Math.floor((Math.random() * 10) + 1);
+    var x = Math.floor((Math.random() * 10));
+    var y = "(Det står velkommen på ";
     document.getElementById("demo").innerHTML = x;
+    document.getElementById("language").innerHTML = y;
 
     switch (x) {
         case 0: //Engelsk
             document.getElementById("demo").innerHTML = "Welcome";
+            document.getElementById("language").innerHTML = y + "engelsk)";
             break;
         case 1: //Fransk
             document.getElementById("demo").innerHTML = "Bienvenue";
+            document.getElementById("language").innerHTML = y + "fransk)";
             break;
         case 2: //Tysk
             document.getElementById("demo").innerHTML = "Willkommen";
+            document.getElementById("language").innerHTML = y + "tysk)";
             break;
         case 3: //Italiensk
             document.getElementById("demo").innerHTML = "Benvenuto";
+            document.getElementById("language").innerHTML = y + "italiensk)";
             break;
         case 4: //SvensK
             document.getElementById("demo").innerHTML = "Välkommen";
+            document.getElementById("language").innerHTML = y + "svensk)";
             break;
         case 5: //Kinesisk (mandarin)
             document.getElementById("demo").innerHTML = "Huānyíng";
+            document.getElementById("language").innerHTML = y + "kinesisk)";
             break;
         case 6: //Finsk
             document.getElementById("demo").innerHTML = "Tervetuloa";
+            document.getElementById("language").innerHTML = y + "finsk)";
             break;
         case 7: //Islandsk
             document.getElementById("demo").innerHTML = "Velkomin";
+            document.getElementById("language").innerHTML = y + "islandsk)";
             break;
         case 8: //Nepalsk
             document.getElementById("demo").innerHTML = "Namaste";
+            document.getElementById("language").innerHTML = y + "nepalsk)";
             break;
         case 9: //Portugisisk
             document.getElementById("demo").innerHTML = "Bem-vindo";
+            document.getElementById("language").innerHTML = y + "portugisisk)";
             break;
         case 10: //Swahili
             document.getElementById("demo").innerHTML = "Karibu"; 
+            document.getElementById("language").innerHTML = y + "swahili)";
             break;
         default:
             document.getElementById("demo").innerHTML = "Velkommen";
+                        document.getElementById("language").innerHTML = y + "norsk)";
+
     }
 }
 
