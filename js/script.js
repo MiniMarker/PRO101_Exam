03f2f78137cd14386b6e0e25b519d94ad2709fa8
MiniMarker <cfmarker@gmail.com>
2017-04-21 12:57:19 +0200

    fikset menybar

--- js/script.js
+++ js/script.js
@@ -1,15 +1,19 @@
 
 // MENUBAR
+    var down = false;
 function showDiv() {
     "use strict";
-    if ((document.getElementById('menubar').style.top) === "-150px") {
+    if((!down)){
+        down = true;
         document.getElementById('menubar').style.top = "0px";
         document.getElementById('page-title').style.opacity = 0;
         document.getElementById('close-menu-button').style.display = "block";
+        
     } else {
         document.getElementById('menubar').style.top = "-150px";
         document.getElementById('page-title').style.opacity = 1;
         document.getElementById('close-menu-button').style.display = "none";
+        down = false;
     }
 }
 
